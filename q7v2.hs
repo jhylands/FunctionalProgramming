9f36aa3ae898ef87db906afa955da4e364319e34
JH Hylands <jhh521@york.ac.uk>
2018-02-24 12:09:31 +0000

    MANAGED Q7!

--- q7v2.hs
+++ q7v2.hs
@@ -4,6 +4,10 @@ instance Show a => Show (Tri a) where
   show (Tri a) =  unlines (pad (map triUnwords a))
 triUnwords xs = unwords (map show xs)
 
+-- the question is asking for a transpose reverse transpose
+
+
+
 pad :: [String]->[String]
 pad [] = []
 pad (x:xs) = (replicate (length xs) ' ' ++ x) : (pad xs)
@@ -13,12 +17,20 @@ trol (Tri xs) = Tri (listTrol xs)
 
 listTrol :: [[a]] -> [[a]]
 listTrol [] = []
-listTrol as = map reverse (listTrans as)
+listTrol as = reverse (listTrans as)
 
 listTrans :: [[a]]->[[a]]
 listTrans [] = []
-listTrans as =  map head as : listTrans (map tail as)
---	     |length (head as) > 0 = map head as : listTrans (map tail as)
+listTrans as = mapHead as : listTrans (mapTail as)
+
+-- the problem is in that we are mapping 
+mapHead :: [[a]] -> [a]
+mapHead [] = []
+mapHead (x:xs) =  if length x >0 then head x : mapHead xs  else mapHead xs 
+
+mapTail :: [[a]] -> [[a]]
+mapTail (x:[]) = if length x > 1 then tail x :[] else []
+mapTail (x:xs) = if length x >0 then tail x : mapTail xs else mapTail xs
 --	     |anyHeadBetter as =  
 --	     |otherwise = []
 
@@ -27,21 +39,29 @@ listTrans as =  map head as : listTrans (map tail as)
 --anyHeadBetter :: [[a]] -> Bool
 --anyHeadBetter as = foldr (+) (map (length . head) as) > 0 
 
---fillSquare :: [[a]] -> [[a]]
+--fillSquare :: [[a]] -> [[(Maybe a)]]
 --fillSquare xs 	= map (arrPad mx) xs where  mx = maximum (map length xs)
-data Pad = Pass | James Int
-instance Show Pad => Show Pad where
-  show Pass = "Pad"
-  show (James x) = show x
 
-arrPad :: Int->[Pad] -> [Pad]
-arrPad mx xs = xs ++(replicate (mx-len) Pass)  where len = length xs
---(
+--sparceSqaure:: [[(Maybe a)]] -> [[a]]
+--sparceSqaure (x:xs) = map 
+
+--j2a :: [Maybe a] -> [a]
+--j2a [(Maybe a):as] = [a]
+--j2a [Nothing] = []
+--j2a [] =[]
+--data Pad = Pass | James Int
+--instance Show Pad => Show Pad where
+--  show Pass = "Pad"
+-- -- show (James x) = show x
+
+--arrPad :: Int->[a] -> [(Maybe a)]
+--arrPad mx xs = (map Just xs) ++(replicate (mx-len) Nothing)  where len = length xs
 
 
 
-max :: Int->Int->Int
-max x y = if x>y then x else y
+
+--max :: Int->Int->Int
+--max x y = if x>y then x else y
 --ben Example
 data Ben a = Ben a
 instance Show a => Show (Ben a) where 
