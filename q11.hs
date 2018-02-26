8d3a40cf065e51b7830d9a933bcaf1ac1c808fa2
JH Hylands <jhh521@york.ac.uk>
2018-02-26 15:04:25 +0000

    managed to finnaly do partitions

--- q11.hs
+++ q11.hs
@@ -1,44 +1,8 @@
---parts :: [a]->[[[a]]]
---parts x = lenSort (segments (a))
-
-segOn :: Int-> [a] -> [[a]]
-segOn _ [] = []
-segOn 1 (x:xs) = [x]:(segOn 1 xs)
-segOn n xs = partition n xs
-
-partition :: Int->[a]->[[a]]
-partition 0 _ = []
-partition n xs |n<=(length xs) = (take n xs):(partition n (drop n xs))
-	       |otherwise = partition (length xs) xs
-
---2 [1,2,3] -> [1,2] [2,3]
-
-partsOflength :: Int->[a]->[[a]]
-partsOflength n xs |(length xs)>=n = (take n xs ):partsOflength n (tail xs)
-		   |otherwise = []
-
-
-segments :: [a]->[[a]]
-
-segments (x:[]) = [x:[]]
-segments (x:xs) = [x]:( (mapXcolon x r) ++ r)  where 
-		r = segments xs
-		h = head xs
---segments (x:xs) =  
-
-mapXcolon :: a->[[a]]->[[a]]
-mapXcolon x (r:[]) = [x:r]
-mapXcolon x r = if that > thiss then (x:(head r) ):(mapXcolon x (tail r)) else [x:(head r)] where 	
-	that = (length . head . tail) r
-	thiss = (length . head) r
-
-lenSort:: [[a]]->[[a]]
-lenSort [] = []
-lenSort [x] = [x]
-lenSort (x:xs) = lenMerge (x:[]) (lenSort xs)
-
-lenMerge :: [[a]]->[[a]]->[[a]]
-lenMerge [] [] = []
-lenMerge [] xs = xs
-lenMerge xs [] = xs
-lenMerge (x:xs) (y:ys) = if (length x) > (length y) then x:lenMerge xs (y:ys) else y:lenMerge (x:xs) ys
+type Partition a = [[a]]
+parts :: [a]->[Partition a]
+parts [] = parts[]
+parts [x] = [[[x]]]
+parts (x:xs) = case1 ++ case2 where 
+	case1 = map ([x]:) ys
+	case2 = map (\(p:ps)->(x:p):ps) ys
+	ys = parts xs 
