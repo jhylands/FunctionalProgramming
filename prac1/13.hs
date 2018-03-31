2d26a7f127a9178ad4d29a4ef49066fd2b3fd615
jhylands <jhylands@server.home>
2018-03-31 19:05:35 +0100

    q13 almost there some problems with negatives

--- prac1/13.hs
+++ prac1/13.hs
@@ -1,8 +1,31 @@
 change :: [Int] -> Int -> [[Int]]
-change coins value = concat map (y coins value) coins
+change [] _ = []
+change _ 0 = [[]]
+change coins value 
+    |value>0 = concat (map z coins) 
+    |otherwise = [[]]
+    where z = (givenThisCoin coins value) 
 
-y :: [Int]->Int->Int->[[Int]]
-y coins value x = change coins (value-x)
+--z :: Int->[[Int]]
+--zomp ::[Int]->int->(Int->[[Int]])
+--zomp = givenThisCoin coins value
 
 
+
+
+givenThisCoin :: [Int]->Int->Int->[[Int]]
+givenThisCoin coins value thisCoin 
+    |value-thisCoin>=0 = map (thisCoin:) (change coins (value-thisCoin))
+
+
+{-
+[1,2] 3 
+
+1: [1,2] 2
+2: [1,2] 1
+1:1: [1,2] 1
+2:1 [1,2] 0
+1:1:1 [1,2] 0
+[[2,1],[1,1,1]]
+-}
 --changeOfaCoin coin value = change coins value-coin
