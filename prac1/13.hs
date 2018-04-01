dd02aac2be785cac843ffb87b76ac6c46694d95a
jhylands <jhylands@server.home>
2018-04-01 10:41:46 +0100

    q13 working solution, could be better

--- prac1/13.hs
+++ prac1/13.hs
@@ -2,20 +2,20 @@ change :: [Int] -> Int -> [[Int]]
 change [] _ = []
 change _ 0 = [[]]
 change coins value 
-    |value>0 = concat (map z coins) 
+    |value>0 = concat (removeBadLeads (map z coins))
     |otherwise = [[]]
     where z = (givenThisCoin coins value) 
 
---z :: Int->[[Int]]
---zomp ::[Int]->int->(Int->[[Int]])
---zomp = givenThisCoin coins value
+--we need some kind of order
+removeBadLeads :: [([[Int]],Bool)]->[[[Int]]]
+removeBadLeads [] = []
+removeBadLeads ((x,y):s) = if y then x:rest  else rest where rest = removeBadLeads s
 
 
-
-
-givenThisCoin :: [Int]->Int->Int->[[Int]]
+givenThisCoin :: [Int]->Int->Int->([[Int]],Bool)
 givenThisCoin coins value thisCoin 
-    |value-thisCoin>=0 = map (thisCoin:) (change coins (value-thisCoin))
+    |value-thisCoin>=0 = (map (thisCoin:) (change [c|c<-coins,c<=thisCoin] (value-thisCoin)),True)
+    |otherwise = (undefined, False)
 
 
 {-
