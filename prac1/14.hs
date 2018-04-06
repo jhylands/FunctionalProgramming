ab713afa715d57c9faa3f7ab605210dca6291288
jhylands <jhylands@server.home>
2018-04-06 20:29:01 +0100

    Working without timegit add -Agit add -Agit add -A!

--- prac1/14.hs
+++ prac1/14.hs
@@ -6,27 +6,29 @@ promptLine output = putStrLn output >> getLine
 main :: IO()
 main = do
     fileName <- promptLine "file:"
-    fileContense <- readFile fileName
-    putStrLn "hello world!"
---    let fileLines = lines fileContense
-    
+    fileLines <- liftM lines (readFile fileName)
+    sum <- liftM resultSum (sequence (itterateThough fileLines repeatAfterMe))
+    printResultSum sum
 
---check :: IO String->String
---check user_input = do user_input  
-    
---main = getLine >>= lines$readFile >>= putStrLn$head
---        putStrLn show (sum$map repeatAfterMe file)
+resultSum :: [(Int,Int)]->(Int,Int)
+resultSum [] = (0,0)
+resultSum ((x,y):xys) = (x+xs,y+ys) where (xs,ys) = resultSum xys
 
+printResultSum :: (Int,Int) -> IO()
+printResultSum (x,y) = putStrLn (show x ++ " : " ++ show y)
 --This is a function which takes a line, and gets the user to copy that line
 --returning the number of words typed and the number of errors
 repeatAfterMe :: String->IO (Int, Int)
-repeatAfterMe x = putStrLn x >> getLine >>= return . \n -> (length$words n ,0)
+repeatAfterMe x = putStrLn x >> getLine >>= return . checkWords x
 
 checkWords :: String->String->(Int,Int)
 checkWords x ln = (length wds,countFalse$zipWith (==) x ln)   
     where wds = words ln
---                 return noty
---                 let (wy,ey) = check x y
+--works like an ordered map function
+itterateThough :: [a]->(a->b)->[b]
+itterateThough [] _ = []
+itterateThough (x:xs) f = f x : itterateThough xs f
+
 countFalse :: [Bool]->Int
 countFalse [] = 0
 countFalse (x:xs)  |x = countFalse xs
@@ -45,7 +47,9 @@ check w:ws r:rs = if w==r then 1+ rst else rst where rst = check ws rs
 --doPureStuff :: [String] -> IO String
 --doPureStuff ln = 
 
---works like an ordered map function
-itterateThough :: [a]->(a->b)->[b]
-itterateThough [] _ = []
-itterateThough (x:xs) f = f x : itterateThough xs f
+--check :: IO String->String
+--check user_input = do user_input  
+    
+--main = getLine >>= lines$readFile >>= putStrLn$head
+--        putStrLn show (sum$map repeatAfterMe file)
+
